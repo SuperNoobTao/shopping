f050877c2529d8fa1e79cd8f3042c69f3d4594e9
SuperNoobTao <387944558@qq.com>
2017-04-04 22:12:39 +0800

    初步完成

--- src/main/java/com/cenyol/example/repository/ProductRepo.java
+++ src/main/java/com/cenyol/example/repository/ProductRepo.java
@@ -19,18 +19,5 @@ public interface ProductRepo extends JpaRepository<ProductEntity, Integer> {
     @Query("select p from ProductEntity p where p.producttype=:producttype")
     public List<ProductEntity> getProductS(@Param("producttype") String id);
 
-    @Modifying // 说明该方法是修改操作
-
-    // @Param注解用于提取参数
-    @Query("update ProductEntity p set p.description=:description, p.imgurl=:imgurl, " +
-                                       "p.price=:price,p.productname=:productname,p.producttype=:producttype," +
-                                       "p.salenum=:salenum  where p.productid=:productid")
-    public void updatePro(@Param("description") String description,
-                           @Param("imgurl") String imgurl,
-                           @Param("price") String price,
-                           @Param("productname") Integer productname,
-                           @Param("producttype") String producttype,
-                           @Param("salenum") int salenum,
-                           @Param("productid") int productid);
 
 }
